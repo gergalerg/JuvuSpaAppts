8e6f824ffee04c50be4d6b3e8c3cf22295c0d971
Simon Forman <forman.simon@gmail.com>
2011-10-19 23:55:56 -0700

    Crude iPad "emulation".

--- beauty/repui/views.py
+++ beauty/repui/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+def index(request):
+    '''
+    Home page.
+    '''
+    c = {}
+    return render_to_response(
+        'index.html',
+        c,
+        context_instance=RequestContext(request),
+        )
+
+
