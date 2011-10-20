0bfdc7aa4fd9a60ca08a7d9fa66d10e7ad659849
Simon Forman <forman.simon@gmail.com>
2011-10-20 00:10:38 -0700

    CSS and serve static media.

--- beauty/urls.py
+++ beauty/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 
 admin.autodiscover()
@@ -8,4 +9,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     url(r'^$', 'repui.views.index', name='home'),
+    (
+        r'^static/(?P<path>.*)$',
+        'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT},
+        ),
 )
