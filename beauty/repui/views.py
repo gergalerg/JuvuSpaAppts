cfbef966e8889290dc285cca7693e4803f8368c4
Simon Forman <forman.simon@gmail.com>
2011-10-20 20:22:18 -0700

    Improving the view and changing the theme.

--- beauty/repui/views.py
+++ beauty/repui/views.py
@@ -2,14 +2,70 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 
+TREATMENTS = '''\
+Microdermabrasion
+Clinical Facial
+Chemical Peel
+Hydrating Therapy
+Deep Cleansing Therapy
+Firming and relaxation
+MicroFacial
+Micropeel
+Oxygen Facial
+Aromatherapy
+Pregnancy Facial
+Dysport
+Perlane
+Juvederm
+Restalyne
+Sculptra
+Intentse Pulsed Light
+Laser Resurfacing
+Acne Treatment
+Sublative Rejuvenation
+Pedicure
+Manicure
+Sclerotherapy
+Endermologie
+Herbal Body wrap
+Infrared Body wrap
+Underarm Rejuvenation
+Intense Pulsed Light
+Skin Tag Removal
+Thermage Skin Tightening
+Back Facial
+Brazillian Facial
+Scrub
+Skin Lightening
+Tattoo removal
+Swedish Massage
+Deep Tissue Massage
+Prenatal Massage
+Thermal Stone Massage
+Shiatsu Massage
+Reflexology
+Laser Hair Removal
+Waxing Hair Removal
+BOTOX Cosmetic
+Accent
+Juvederm
+Radiesse
+Perlane
+Pixel Skin Treatment
+PhotoDynamic Therapy
+Skin Tightening
+PhotoFacial Elite
+Photorejuvenation
+Fraxel'''.splitlines()
+
+
 def index(request):
     '''
     Home page.
     '''
-    c = {}
     return render_to_response(
         'index.html',
-        c,
+        dict(treatments=TREATMENTS),
         context_instance=RequestContext(request),
         )
 
