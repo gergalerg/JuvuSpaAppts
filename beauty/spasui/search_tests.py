76a7ad74c343cbe53acef2fbb825fa0e5bff8af9
Simon Forman <forman.simon@gmail.com>
2011-11-08 14:53:27 -0800

    Put tests in the right places.

--- beauty/spasui/search_tests.py
+++ beauty/spasui/search_tests.py
@@ -5,7 +5,6 @@ except ImportError:
     from fake_rdf import Storage, Model, Statement
 from django.test import TestCase
 from django.conf import settings
-from spasui.search import process_POST_params
 from spasui.models import (
     add_treatment_to_trenche,
     create_availability_with_trenche,
@@ -25,67 +24,6 @@ from beauty.util.URIs import (
     AVAIL,
     )
 import spasui.models
-
-
-class process_POST_params_Test(TestCase):
-
-    class FakeRequest:
-        def __init__(self):
-            self.POST = {
-                u'csrfmiddlewaretoken': [u'db4187ab0c4fe8ef8859b2e9948ecaba'],
-                u'location': [u'San Francisco'],
-                u'treatment': [u'massage'],
-                u'today': [u'on'],
-                u'anotherday': [u'']
-                }
-
-    def setUp(self):
-        self.fake_request = self.FakeRequest()
-
-    def test_date(self):
-        today = date.today()
-
-        # Find today as per the request params above.
-        result = process_POST_params(self.fake_request)
-        self.assertEqual(result['dates'], [today])
-
-        # Add tomorrow.
-        self.fake_request.POST[u'tomorrow'] = [u'on']
-        result = process_POST_params(self.fake_request)
-        self.assertEqual(
-            result['dates'],
-            [today, today + timedelta(days=1)]
-            )
-
-        # Remove today.
-        del self.fake_request.POST[u'today']
-        result = process_POST_params(self.fake_request)
-        self.assertEqual(
-            result['dates'],
-            [today + timedelta(days=1)]
-            )
-
-        # Add a specific date (in the past, as the business logic of not
-        # allowing appointments in the past will not have been applied
-        # yet.
-        self.fake_request.POST[u'anotherday'] = [u'11/28/2010']
-        result = process_POST_params(self.fake_request)
-        self.assertEqual(
-            result['dates'],
-            [today + timedelta(days=1), date(2010, 11, 28)]
-            )
-
-    def test_location(self):
-        result = process_POST_params(self.fake_request)
-
-        self.assertEqual(
-            result['lat_long'],
-            {u'lat': 37.7749295, u'lng': -122.4194155}
-            )
-        self.assertEqual(
-            result['location_full_text'],
-            u'San Francisco, CA, USA'
-            )
 
 
 class ModelMixin:
--- beauty/spasui/tests.py
+++ beauty/spasui/tests.py
@@ -1,23 +1,66 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
+from datetime import date, timedelta
 from django.test import TestCase
+from spasui.search import process_POST_params
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+class process_POST_params_Test(TestCase):
 
->>> 1 + 1 == 2
-True
-"""}
+    class FakeRequest:
+        def __init__(self):
+            self.POST = {
+                u'csrfmiddlewaretoken': [u'db4187ab0c4fe8ef8859b2e9948ecaba'],
+                u'location': [u'San Francisco'],
+                u'treatment': [u'massage'],
+                u'today': [u'on'],
+                u'anotherday': [u'']
+                }
+
+    def setUp(self):
+        self.fake_request = self.FakeRequest()
+
+    def test_date(self):
+        today = date.today()
+
+        # Find today as per the request params above.
+        result = process_POST_params(self.fake_request)
+        self.assertEqual(result['dates'], [today])
+
+        # Add tomorrow.
+        self.fake_request.POST[u'tomorrow'] = [u'on']
+        result = process_POST_params(self.fake_request)
+        self.assertEqual(
+            result['dates'],
+            [today, today + timedelta(days=1)]
+            )
+
+        # Remove today.
+        del self.fake_request.POST[u'today']
+        result = process_POST_params(self.fake_request)
+        self.assertEqual(
+            result['dates'],
+            [today + timedelta(days=1)]
+            )
+
+        # Add a specific date (in the past, as the business logic of not
+        # allowing appointments in the past will not have been applied
+        # yet.
+        self.fake_request.POST[u'anotherday'] = [u'11/28/2010']
+        result = process_POST_params(self.fake_request)
+        self.assertEqual(
+            result['dates'],
+            [today + timedelta(days=1), date(2010, 11, 28)]
+            )
+
+    def test_location(self):
+        result = process_POST_params(self.fake_request)
+
+        self.assertEqual(
+            result['lat_long'],
+            {u'lat': 37.7749295, u'lng': -122.4194155}
+            )
+        self.assertEqual(
+            result['location_full_text'],
+            u'San Francisco, CA, USA'
+            )
+
 
