8545587be61b76082e80c0f32666652c33e1aabb
Simon Forman <forman.simon@gmail.com>
2011-11-11 22:23:50 -0800

    Lessify results.css and clean up urls.py

--- beauty/urls.py
+++ beauty/urls.py
@@ -1,26 +1,10 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
-from django.views.generic.simple import direct_to_template
+
 
 admin.autodiscover()
 
-COLORS = dict(
-    bg='#eeffee',
-
-    header='#003300',
-    header_bg='#fff',
-
-    main='#003300',
-    main_bg='#dfd',
-
-    results='#003300',
-    results_bg='#fff',
-
-    tools='#003300',
-    tools_bg='#fff',
-
-    )
 
 urlpatterns = patterns('',
 
@@ -29,26 +13,6 @@ urlpatterns = patterns('',
 
     # User-facing site, 'uui' app.
     url(r'^$', 'uui.views.home', name='home'),
-    ( # Actually, this is the CSS for 'home'.
-        r'^dyn/search.css$',
-        direct_to_template,
-        dict(
-            template='search.css',
-            extra_context=dict(colors=COLORS),
-            mimetype='text/css',
-            ),
-        ),
-
-    (
-        r'^dyn/results.css$',
-        direct_to_template,
-        dict(
-            template='results.css',
-            extra_context=dict(colors=COLORS),
-            mimetype='text/css',
-            ),
-        ),
-
     url(r'^search/', 'uui.views.search', name='search'),
     url(r'^booking/', 'uui.views.booking', name='booking'),
     url(r'^confirmation/', 'uui.views.confirmation', name='confirmation'),
