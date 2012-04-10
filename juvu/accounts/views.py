58a4116ebe4f07a7be45cc0c63b76c8424fe3fcb
Simon Forman <forman.simon@gmail.com>
2012-04-10 15:11:23 -0700

    Signup functionality

--- juvu/accounts/views.py
+++ juvu/accounts/views.py
@@ -1,6 +1,15 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response, redirect
 from django.contrib import auth
+from django.contrib.auth.models import User
+from django import forms
+
+
+class UserSignupForm(forms.ModelForm):
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
 
 
 def login(request):
@@ -14,3 +23,33 @@ def login(request):
         auth.login(request, user)
         return redirect("/Hooray!")
     return redirect('/invalid_login')
+
+
+def signup(request):
+    if request.method != 'POST':
+        form = UserSignupForm()
+    else:
+        form = UserSignupForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+                form.cleaned_data['email'],
+                form.cleaned_data['email'],
+                form.cleaned_data['password'],
+                )
+            user.first_name = form.cleaned_data['first_name']
+            user.last_name = form.cleaned_data['last_name']
+            user.save()
+            user = authenticate(
+                username=form.cleaned_data['email'],
+                password=form.cleaned_data['password'],
+                )
+            login(request, user)
+            return HttpResponseRedirect('/#step/0')
+
+    return render_to_response(
+        'signup.html',
+        dict(form=form),
+        context_instance=RequestContext(request),
+        )
+
+
